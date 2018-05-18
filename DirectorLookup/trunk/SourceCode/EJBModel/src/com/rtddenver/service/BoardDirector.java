083ff3a0339a16e6ee98f408ea6333fb5129269a
Jay Butler <jay.butler@rtd-denver.com>
2018-05-18 13:06:43 +0000

    Removed reference to schema, which is now defined in the DS in WLS.  INC0000000

--- DirectorLookup/trunk/SourceCode/EJBModel/src/com/rtddenver/service/BoardDirector.java
+++ DirectorLookup/trunk/SourceCode/EJBModel/src/com/rtddenver/service/BoardDirector.java
@@ -2,29 +2,37 @@ package com.rtddenver.service;
 
 import java.io.Serializable;
 
-import java.sql.Timestamp;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.IdClass;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+/**
+ * To create ID generator sequence BOARDDIRECTOR_ID_SEQ_GEN:
+ * CREATE SEQUENCE BOARDDIRECTOR_ID_SEQ_GEN INCREMENT BY 50 START WITH 50;
+ */
 @Entity
 @NamedQueries({ @NamedQuery(name = "findDirectorByDistrict", query = "select o from BoardDirector o " +
     "WHERE UPPER(o.district) = UPPER(:district) AND UPPER(o.active) = 'Y' "),
                 @NamedQuery(name = "getAllDirectors", query = "select o from BoardDirector o " +
     "WHERE UPPER(o.active) = 'Y'") })
-@Table(name = "BOARD_DIRECTOR", schema = "BOARD_DIR")
+@Table(name = "BOARD_DIRECTOR")
+@SequenceGenerator(name = "BoardDirector_Id_Seq_Gen", sequenceName = "BOARDDIRECTOR_ID_SEQ_GEN", allocationSize = 50,
+                   initialValue = 50)
 
 public class BoardDirector implements Serializable {
+    @SuppressWarnings("compatibility:3809877802474394801")
     private static final long serialVersionUID = -5565086281767852537L;
     @Column(name = "ACTIVE", nullable = false, length = 1)
     private String active;
     @Id
     @Column(name = "DIRECTOR_FIRST_NAME", nullable = false, length = 30)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BoardDirector_Id_Seq_Gen")
     private String directorFirstName;
     @Id
     @Column(name = "DIRECTOR_LAST_NAME", nullable = false, length = 30)
